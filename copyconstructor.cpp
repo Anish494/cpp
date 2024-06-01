5aa4ce53a1589dfe415e3988da93728930fcfd43
Anish <anishks494@gmail.com>
2024-06-01 21:02:54 +0545

    added const

--- copyconstructor.cpp
+++ copyconstructor.cpp
@@ -1,6 +1,37 @@
 #include<iostream>
 using namespace std;
+class Number{
+    int a;
+    public:
+        Number(){
+            a=0;
+        }
+        Number(int num){
+            a=num;
+        }
+        Number(Number &obj){                //reference to obj is passed
+            cout<<"Copy constructor called ."<<endl;
+            a=obj.a;                         //obj.a is assigned to a
+        }
+        void display(){
+            cout<<"The value of number for this object is "<<a<<endl;
+        }
+};
 int main(){
+    // Number x=Number(3);
+    // x.display();
+
+    Number x,y,z(8);
+    x.display();               //throws garbage value if default constructor doesnot assign any value
+                                //since its data is not set during declaration/initialization
     
+    y.display();
+    z.display();
+
+    //if you need an object with same property/value as z, we can make it using copyconstructor for z1 and pass
+    //z to it
+
+    Number z1(z);
+    z1.display();
     return 0;
 }
